78c012a34382dac2ac128c65506fc8d95b76714c
Kyle Huang <kylehuang06@gmail.com>
2025-05-07 15:30:12 -0700

    Update read pwm

--- Inductive Sensor/read_pwm.py
+++ Inductive Sensor/read_pwm.py
@@ -4,9 +4,9 @@ pi = pigpio.pi()
 
 def read_spi():
     spi = pi.spi_open(0, 1_600_000, 0)  # SPI mode 0 = 0, 0
-    data_1 = pi.spi_read(spi, 1)  
-    data_2 = pi.spi_read(spi, 2) 
-    data = ((int(data_1) & 0x1F) << 7) + ((int(data_2) & 0xFE) >> 1)
+    _, data_1 = pi.spi_read(spi, 1)  
+    _, data_2 = pi.spi_read(spi, 2) 
+    data = ((data_1 & 0x1F) << 7) + ((data_2 & 0xFE) >> 1)
     pi.spi_close(spi)
     return data
 
