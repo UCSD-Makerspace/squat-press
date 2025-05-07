feece8b262f5589295927c700b679cec37076116
Kyle Huang <kylehuang06@gmail.com>
2025-05-07 15:26:40 -0700

    convert tuple to int

--- Inductive Sensor/read_pwm.py
+++ Inductive Sensor/read_pwm.py
@@ -6,7 +6,7 @@ def read_spi():
     spi = pi.spi_open(0, 1_600_000, 0)  # SPI mode 0 = 0, 0
     data_1 = pi.spi_read(spi, 1)  
     data_2 = pi.spi_read(spi, 2) 
-    data = ((data_1 & 0x1F) << 7) + ((data_2 & 0xFE) >> 1)
+    data = ((int(data_1) & 0x1F) << 7) + ((int(data_2) & 0xFE) >> 1)
     pi.spi_close(spi)
     return data
 
