fbc5ef52cdb164383afd8bb83c3cd7f23630d14e
Kyle Huang <kylehuang06@gmail.com>
2025-05-14 15:16:44 -0700

    DAC

--- InductiveSensor/DAC.py
+++ InductiveSensor/DAC.py
@@ -4,17 +4,16 @@ import time
 pi = pigpio.pi()
 
 class DAC:
-    def __init__(self, pi=None, clk=1_600_000, channel=0):
+    def __init__(self, pi=None, clk=1_600_000):
         if pi is None:
             self.pi = pigpio.pi()
         else:
             self.pi = pi
-        self.channel = channel
         self.clk = clk
         self.raw_data = 0
 
     def read(self):
-        spi = pi.spi_open(self.channel, self.clk, 0)  # SPI mode 0 = 0, 0
+        spi = pi.spi_open(0, self.clk, 0)  # SPI mode 0 = 0, 0
         _, data_1 = pi.spi_read(spi, 2)  
         # data = ((data_1[0] & 0x1F) << 7) + ((data_1[1] & 0xFE) >> 1)
         pi.spi_close(spi)
