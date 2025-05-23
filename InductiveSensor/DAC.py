acfce2565863f5b3d9bab08276023ad75e7c9f41
Kyle Huang <kylehuang06@gmail.com>
2025-05-23 15:49:18 -0700

    Photo Interruptor

--- InductiveSensor/DAC.py
+++ InductiveSensor/DAC.py
@@ -10,26 +10,26 @@ class DAC:
         else:
             self.pi = pi
         self.clk = clk
-        self.raw_data: bytearray = bytearray(2) # 12-bit = 1.5 bytes
-        self.data = 0
+        self._raw_data: bytearray = bytearray(2) # 12-bit = 1.5 bytes
+        self._data = 0
 
     def update(self) -> bytearray:
         spi = self.pi.spi_open(0, self.clk, 0)  # SPI mode 0 = 0, 0
         _, data_1 = self.pi.spi_read(spi, 2)  
         # data = ((data_1[0] & 0x1F) << 7) + ((data_1[1] & 0xFE) >> 1)
         self.pi.spi_close(spi)
-        self.raw_data = data_1
-        self.data = int.from_bytes(data_1, byteorder="big")
+        self._raw_data = data_1
+        self._data = int.from_bytes(data_1, byteorder="big")
         return data_1
     
     def get_data(self) -> int:
-        return self.data
+        return self._data
     
     def get_data_percent(self) -> float:
-        return self.data / (MAX_VAL)
+        return self._data / (MAX_VAL)
 
     def get_raw_data(self) -> bytearray:
-        return self.raw_data
+        return self._raw_data
 
 if __name__ == "__main__":
     reader = DAC()
---
+++ InductiveSensor/PhotoInterruptor.py
@@ -0,0 +1,25 @@
+from DAC import DAC
+
+
+class PhotoInterruptor(DAC):
+    def __init__(self, pi=None, clk=1_600_000, threshold=0.08) -> None:
+        super().__init__(pi, clk)
+        self._detected = False
+        self.threshold = threshold
+
+    def get_detected(self) -> bool:
+        return self._detected
+
+    def update(self) -> bytearray:
+        super().update()
+        self._detected = self._data > self.threshold
+        return self._detected
+
+
+if __name__ == "__main__":
+    import time
+    sensor = PhotoInterruptor()
+    while True:
+        sensor.update()
+        print(f"Detected: {sensor.get_detected()}Data: {sensor.get_data_percent():0.5f}")
+        time.sleep(0.05)
