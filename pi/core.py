f2ddff1adb7a09b99939369d595eb7123b041bcf
hermano727 <hh727w@gmail.com>
2025-08-11 12:29:59 -0700

    Begin writing core logic

--- pi/core.py
+++ pi/core.py
@@ -4,6 +4,7 @@ import pigpio
 import threading
 from os import path
 from tests.config import SystemConfig
+from tests.IntegrationTest import test_linear_sensor, test_photo_interruptor, test_motor
 from time import sleep
 import logging
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
@@ -14,20 +15,42 @@ import ADC.ADC as ADC
 
 #### Consts ####
 SENT_GPIO = 18
+TEST_LINEAR_SENSOR = False
+TEST_PHOTO_INTERRUPTOR = True
+TEST_MOTOR = True
+
+def dispense_pellet():
+    logging.info("Lift detected: dispensing pellet.")
 
 # Core loop for hardware communication #
 def main():
+    config = SystemConfig()
     pi = pigpio.pi()
     p = SENTReader.SENTReader(pi, SENT_GPIO)
 
-    # Needed functions
-    motor.rotate()
-    
-    pellet.dispense()
-    
+    threads = []
 
-    beambreak.record_event()
+    try:
+        if TEST_LINEAR_SENSOR:
+            thread = threading.Thread(target=test_linear_sensor, args=(pi, config))
+            threads.append(thread)
+            thread.start()
+        if TEST_PHOTO_INTERRUPTOR:
+            thread = threading.Thread(target=test_photo_interruptor, args=(pi, config))
+            threads.append(thread)
+            thread.start()
+        if TEST_MOTOR:
+            thread = threading.Thread(target=test_motor, args=())
+            threads.append(thread)
+            thread.start()
+            
+        for thread in threads:
+            thread.join()
+    except KeyboardInterrupt:
+        logging.info("Tests interrupted by user.")
+    finally:
+        pi.stop()
+        logging.info("Tests completed. Cleaning up...")
 
 if __name__ == "__main__":
-    
     main()
