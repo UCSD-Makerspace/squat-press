ca2f453b5d4e491d59e4b64dfb53b45234294231
hermano727 <hh727w@gmail.com>
2025-10-02 12:01:14 -0700

    use python round

--- tests/Linear Sensor/unit_test.py
+++ tests/Linear Sensor/unit_test.py
@@ -1,4 +1,3 @@
-import math
 import Dispenser.TMC2209.tmc2209 as tmc2209
 import lx3302a.SENTReader.serial_reader as serial_reader
 import matplotlib.pyplot as plt
@@ -7,7 +6,7 @@ import time
 import csv
 
 SAMPLE_INTERVAL = 0.1
-STEPS_PER_2_5CM = math.round(916 / (64/16))  # steps for 2.5cm motion
+STEPS_PER_2_5CM = round(916 / (64/16))  # steps for 2.5cm motion
 
 def check_mm_value(sensor, mm_value, since_last_mm):
     """Return (interpolated mm, since_last_mm, raw decimal value)"""
