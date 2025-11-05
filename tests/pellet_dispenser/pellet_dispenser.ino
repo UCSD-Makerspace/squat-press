cfd1686cbe5b2301ef50ddaabe35dd59d2a179a5
Kyle Huang <kylehuang06@gmail.com>
2025-11-05 14:12:33 -0800

    Pellet

---
+++ tests/pellet_dispenser/pellet_dispenser.ino
@@ -0,0 +1,47 @@
+#include <Arduino.h>
+#include "A4988.h"
+
+#define MOTOR_STEPS 200
+#define RPM 10
+
+// Pin setup
+#define DIR 18
+#define STEP 19
+
+// #define MS1 22
+// #define MS2 23
+// #define MS3 21
+#define SLP 5
+
+// Microstepping mode
+#define MICROSTEPS 1
+
+A4988 stepper(MOTOR_STEPS, DIR, STEP, SLP);
+
+void setup() {
+    Serial.begin(115200);
+    delay(1000);
+    stepper.begin(RPM);
+    stepper.setEnableActiveState(LOW);
+    stepper.enable();
+    // stepper.setMicrostep(MICROSTEPS);
+    Serial.println("Stepper ready.");
+    delay(1000);
+}
+
+void loop() {
+    if (Serial.available() > 0) {
+        String input = Serial.readStringUntil('\n');
+        input.trim();
+
+        if (input.length() > 0) {
+            int degrees = input.toInt();
+
+            Serial.print("Received command: ");
+            Serial.print(degrees);
+
+            stepper.rotate(degrees);
+        }
+    }
+    delay(1000);
+}
