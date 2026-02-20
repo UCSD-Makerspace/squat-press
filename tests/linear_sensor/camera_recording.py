5ac9924f5d748d366eb7eb31432fadfbebe65d79
Hermano727 <hh727w@gmail.com>
2026-02-20 13:47:18 -0800

    Feat: update camera script to use open cv

--- tests/linear_sensor/camera_recording.py
+++ tests/linear_sensor/camera_recording.py
@@ -1,6 +1,20 @@
-from picamera2 import PiCamera2
-from time import sleep
+import cv2
 
-camera = PiCamera2()
+# OV7670 index should be 0
+cap = cv2.VideoCapture(0)
 
-camera.start_preview()
+if not cap.isOpened():
+    print("Cannot open camera. Check wiring and dtoverlay.")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    
+    cv2.imshow('OV7670 Feed', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
