1f0f965f5dc476e99053d0a3d7510a67143089f5
Hermano727 <hh727w@gmail.com>
2026-02-20 15:58:57 -0800

    Increase fps cap

--- tests/linear_sensor/camera_recording.py
+++ tests/linear_sensor/camera_recording.py
@@ -6,13 +6,14 @@ cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
 
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+cap.set(cv2.CAP_PROP_FPS, 30)
 
 fourcc = cv2.VideoWriter_fourcc(*'mp4v') 
 now = datetime.now()
 timestamp = now.strftime("%Y%m%d_%H%M%S")
 
 filename = f'squat_recording_{timestamp}.mp4'
-out = cv2.VideoWriter(filename, fourcc, 20.0, (1280, 720))
+out = cv2.VideoWriter(filename, fourcc, 30.0, (1280, 720))
 
 print("REC started... Press Ctrl+C to stop and save.")
 
