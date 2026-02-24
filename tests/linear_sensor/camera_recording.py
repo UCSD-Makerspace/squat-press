e77ced57f4deb0c35b69ddb3a99cc9f905e6bac1
Hermano727 <hh727w@gmail.com>
2026-02-24 11:54:25 -0800

    Feat: update cam settings:

--- tests/linear_sensor/camera_recording.py
+++ tests/linear_sensor/camera_recording.py
@@ -2,24 +2,22 @@ import cv2
 from datetime import datetime
 import sys
 
-cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
+cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
 
 cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-cap.set(cv2.CAP_PROP_FPS, 30)
 
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-actual_fps = cap.get(cv2.CAP_PROP_FPS)
 
 print(f"Hardware: {width}x{height} @ {actual_fps} FPS")
 
-fourcc = cv2.VideoWriter_fourcc(*'XVID') 
+fourcc = cv2.VideoWriter_fourcc(*'mp4v') 
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-filename = f'squat_recording_{timestamp}.avi'
+filename = f'squat_recording_{timestamp}.mp4'
 
-out = cv2.VideoWriter(filename, fourcc, actual_fps, (width, height))
+out = cv2.VideoWriter(filename, fourcc, 30, (width, height))
 
 if not out.isOpened():
     print("Error: VideoWriter could not be opened.")
